9e8749e71235c1e40e62218e499400ce5297c078
Grzegorz Mazurek <grzegorz.mazurek86@gmail.com>
2016-12-10 22:20:35 +0100

    #formatCODE

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -1,6 +1,6 @@
 public class Point {
 
-	public static void main (String [] args) {
-		System.out.println("hello :)");
-	}
+  public static void main(String[] args) {
+    System.out.println("hello :)");
+  }
 }
