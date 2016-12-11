6af5cd73b21c8659338f127f9b4aab2062a1c242
Grzegorz Mazurek <grzegorz.mazurek86@gmail.com>
2016-12-11 16:45:38 +0100

    #lst

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,7 @@
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-       // System.out.println("hello :)");
+       /// System.out.println("hello :)");
     Point p = new Point(10,6);
     System.out.println("distance beetwen point p1  " +p.p1 +" and point p2 " + p.p2 +"= " +  p.distance() );
 
