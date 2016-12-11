5a320a0d966fd5c9b062d98d5a645926816d0d1d
Grzegorz Mazurek <grzegorz.mazurek86@gmail.com>
2016-12-11 16:42:50 +0100

    #testy1

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,9 +1,9 @@
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("hello :)");
-    Point p = new Point(10,5);
-    System.out.println("distance beetwen point p1  " +p.p1 +"and point p2" + p.p2 +"=" +  p.distance() );
+       // System.out.println("hello :)");
+    Point p = new Point(10,6);
+    System.out.println("distance beetwen point p1  " +p.p1 +" and point p2 " + p.p2 +"= " +  p.distance() );
 
 
     }
