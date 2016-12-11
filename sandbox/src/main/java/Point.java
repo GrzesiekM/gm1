155750ec43e4e502f777d8af1e811905fe8b96d9
Grzegorz Mazurek <grzegorz.mazurek86@gmail.com>
2016-12-11 16:40:59 +0100

    #test

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -1,6 +1,18 @@
+/**
+ * Created by ThinkPad on 10.12.2016.
+*/
 public class Point {
 
-  public static void main(String[] args) {
-    System.out.println("hello :)");
+  public double p1;
+  public double p2;
+
+  public Point(double p1, double p2) {
+    this.p1 = p1;
+    this.p2 = p2;
   }
+
+  public double distance() {
+    return this.p1 - this.p2;
+  }
+
 }
